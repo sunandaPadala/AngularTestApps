ba48845b434c07ca0d54d7479bb347876dfa374d
Sukanya <marrapu.sukanya16@gmail.com>
2021-05-17 20:07:04 +0530

    added_header and side_nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import {MenuModule} from 'primeng/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageViewComponent } from './page-view/page-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    PageViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ToolbarModule,
+    ButtonModule,
+    MenuModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,25 @@
+import { Component,OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  items: MenuItem[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {label: 'Sample Table'},
+      {label: 'Pagination Table'},
+      {label: 'Forms'},
+      {label: 'Forms with Upload'}
+  ];
+
+  }
+
+}
