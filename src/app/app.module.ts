a0bf8b794c3e57cbf8dfb48ee2372b28add1f137
Sukanya <marrapu.sukanya16@gmail.com>
2021-05-18 12:43:43 +0530

    added header-sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { PageComponent } from './page/page.component';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import {MenuModule} from 'primeng/menu';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ToolbarModule,
+    ButtonModule,
+    MenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
