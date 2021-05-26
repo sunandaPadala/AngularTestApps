e0aac08cff50bbbd9f8a8997b6a07d1360b2405f
SwathiChalumuri <swathi.chalumuri9@gmail.com>
2021-05-26 16:43:02 +0530

    used input decorator

--- src/app/components/lanes-child/lanes-child.component.ts
+++ src/app/components/lanes-child/lanes-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lanes-child',
@@ -6,24 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lanes-child.component.scss']
 })
 export class LanesChildComponent implements OnInit {
-  arr = [
-    {
-    header1: '3',
-    text1: 'Carrier Rates Pending Approval',
-    url1: '../assets/pending.png',
-    header2: '250',
-    text2: 'Approved Carrier Rates',
-    url2: '../assets/approved.png'
-    },
-    {
-      header1: '4',
-      text1: 'Carrier Rates in Disputes',
-      url1: '../assets/disputes.png',
-      header2: '500',
-      text2: 'All My Lanes',
-      url2: '../assets/lanes.png'
-    },
-    ]
+  @Input() image;
+  @Input() header;
+  @Input() text;
   constructor() { }
 
   ngOnInit(): void {
